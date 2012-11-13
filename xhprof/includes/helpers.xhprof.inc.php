0d906a51ce5476a7cd67e566b8b2b00392df9cb3
Akshay Joshi <me@akshayjoshi.com>
2012-11-13 11:14:47 -0800

    Fix mu/µ.

--- xhprof/includes/helpers.xhprof.inc.php
+++ xhprof/includes/helpers.xhprof.inc.php
@@ -7,19 +7,19 @@ function url($template = NULL, array $xhprof_query = NULL, array $xhprof = array
 	{
 		return BASE_URL;
 	}
-	
+
 	$query	= array
 	(
 		'xhprof'	=> $xhprof
 	);
-	
+
 	$query['xhprof']['template']	= $template;
-	
+
 	if($xhprof_query)
 	{
 		$query['xhprof']['query']	= $xhprof_query;
 	}
-	
+
 	// Technically, this invalidates the URL. However, I prefer a readable URL.
 	return BASE_URL . '?' . str_replace(array('%5B', '%5D'), array('[', ']'), http_build_query($query));
 }
@@ -32,19 +32,19 @@ function uuid()
 	return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
 		// 32 bits for "time_low"
 		mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
-		
+
 		// 16 bits for "time_mid"
 		mt_rand( 0, 0xffff ),
-		
+
 		// 16 bits for "time_hi_and_version",
 		// four most significant bits holds version number 4
 		mt_rand( 0, 0x0fff ) | 0x4000,
-		
+
 		// 16 bits, 8 bits for "clk_seq_hi_res",
 		// 8 bits for "clk_seq_low",
 		// two most significant bits holds zero and one for variant DCE1.1
 		mt_rand( 0, 0x3fff ) | 0x8000,
-		
+
 		// 48 bits for "node"
 		mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
 	);
@@ -56,7 +56,7 @@ function calculate_percentage_change($original_value, $new_value)
 	{
 		return 'undefined';
 	}
-	
+
 	return format_number((($new_value-$original_value)/$original_value)*100) . '%';
 }
 
@@ -68,27 +68,27 @@ function format_number($number)
 {
 	$multiplier	= 100;
 	$cap		= 5;
-	
+
 	$value		= 0;
-	
+
 	while($value == 0)
 	{
 		$value		= floor($number*$multiplier)/$multiplier;
-		
+
 		if(!--$cap)
 		{
 			// the number is too small to be relavent
 			break;
 		}
-		
+
 		$multiplier	= 10*$multiplier;
 	}
-	
+
 	if( strpos((string) $value, 'E') !== FALSE )
 	{
 		return rtrim(sprintf('%F', $value), 0);
 	}
-	
+
 	return $value;
 }
 
@@ -98,10 +98,10 @@ function format_bytes($size, $precision = 2, $format = TRUE)
 	{
 		return 0;
 	}
-	
+
     $base		= log(abs($size)) / log(1024);
     $suffixes	= array('b', 'k', 'M', 'G', 'T');
-    
+
     $number		= round(pow(1024, $base - floor($base)), $precision);
     $suffix		= $suffixes[floor($base)];
 
@@ -113,20 +113,20 @@ function format_microseconds($time, $format = TRUE)
 	$time	= (int) $time;
 
 	$pad	= FALSE;
-	$suffix	= 'µs';
+	$suffix	= '&#956;s'; // This is "µs".
 
 	if (abs($time) >= 1000)
 	{
 		$time	= $time / 1000;
 		$suffix	= 'ms';
-		
+
 		if (abs($time) >= 1000)
 		{
 			$pad	= TRUE;
-			
+
 			$time	= $time / 1000;
 			$suffix	= 's';
-			
+
 			if (abs($time) >= 60)
 			{
 				$time	= $time / 60;
@@ -134,12 +134,12 @@ function format_microseconds($time, $format = TRUE)
 			}
 		}
 	}
-	
+
 	if($pad)
 	{
 		$time	= sprintf('%.4f', $time);
 	}
-	
+
 	return $format ? '<span class="value">' . $time . '</span> <span class="measure">' . $suffix . '</span>' : $time . ' ' . $suffix;
 }
 
@@ -151,14 +151,14 @@ function format_metrics(array $data)
 	$format	= array
 	(
 		'request_count'	=> '\number_format',
-		
+
 		'ct'			=> 'ay\xhprof\format_number',
 		'wt'			=> 'ay\xhprof\format_microseconds',
 		'cpu'			=> 'ay\xhprof\format_microseconds',
 		'mu'			=> 'ay\xhprof\format_bytes',
 		'pmu'			=> 'ay\xhprof\format_bytes'
 	);
-	
+
 	foreach($data as $k => $v)
 	{
 		if(isset($format[$k]))
@@ -166,7 +166,7 @@ function format_metrics(array $data)
 			$data[$k]	= array('raw' => $v, 'formatted' => call_user_func($format[$k], $v));
 		}
 	}
-	
+
 	return $data;
 }
 
@@ -175,14 +175,14 @@ function validate_datetime($input)
 	$format	= 'Y-m-d H:i:s';
 
 	$date	= DateTime::createFromFormat($format, $input);
-	
+
 	if($date === FALSE)
 	{
 		$format	= 'Y-m-d';
-	
+
 		$date	= DateTime::createFromFormat($format, $input);
 	}
-	
+
 	return (boolean) $date;
 }
 
