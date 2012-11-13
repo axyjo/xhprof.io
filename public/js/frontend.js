f61696a5128a7e3b0e1e7bc85af54b994e47d56c
Akshay Joshi <me@akshayjoshi.com>
2012-11-13 10:58:38 -0800

    Make buttons toggle instead of being removed.

--- public/js/frontend.js
+++ public/js/frontend.js
@@ -1,35 +1,28 @@
 $(function(){
 	$('#navigation .button-filter').on('click', function(){
-		$(this).remove();
-		
-		$('#filter').show();
-	});
-	
-	$('#navigation .button-summary').on('click', function(){
-		$(this).remove();
-		
-		$('#metrics-summary').show();
+		$('#filter').toggle();
 	});
 
-	$('table.ay-sort').ayTableSort();
-	$('thead.ay-sticky').ayTableSticky();
-	
+	$('#navigation .button-summary').on('click', function(){
+		$('#metrics-summary').toggle();
+	});
+
 	if($('table.aggregated-callstack').length)
 	{
 		var alternate	= $('[data-ay-alternate]');
-	
+
 		alternate.on('ay-alternate', function(e, stage){
 			var data	= $(this).data('ay-alternate');
-			
+
 			$(this).data('ay-alternate-stage', stage);
-			
+
 			$(this).html(data[stage]);
 		});
-		
+
 		alternate.on('click', function(){
 			var data	= $(this).data('ay-alternate');
 			var stage	= $(this).data('ay-alternate-stage');
-			
+
 			if(typeof stage == 'undefined')
 			{
 				stage	= 0;
@@ -42,7 +35,7 @@ $(function(){
 			{
 				stage	= 0;
 			}
-			
+
 			alternate.trigger('ay-alternate', stage);
 		});
 	}
